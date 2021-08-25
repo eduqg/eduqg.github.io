f2a1bce605c10f5b1ccbac405e3d1ccbf92b2af1
eduqg <eduardoqgomes@gmail.com>
2021-08-25 18:06:27 -0300

    Tag manager

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,15 @@
 import { AppProps } from 'next/app'
+import { useEffect } from 'react'
 
 import GlobalStyle from '../styles/GlobalStyles'
 
+import TagManager from 'react-gtm-module'
+
 function MyApp({ Component, pageProps }: AppProps): JSX.Element {
+  useEffect(() => {
+    TagManager.initialize({ gtmId: process.env.NEXT_PUBLIC_GTM_ID })
+  }, [])
+
   return (
     <>
       <GlobalStyle />
