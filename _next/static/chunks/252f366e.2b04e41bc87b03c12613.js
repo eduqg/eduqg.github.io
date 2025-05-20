79a64c7a6a979b62d5fd5f16c2a5f9dc481c2f0d
eduqg <eduardoqgomes@gmail.com>
2025-05-20 18:17:52 +0000

    Deploying to gh-pages from @ eduqg/eduqg.github.io@3eb9c8411fc7939eb12b90d82dde416e4c0fa561 🚀

--- _next/static/chunks/252f366e.2b04e41bc87b03c12613.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[228],{5434:function(t,c,n){"use strict";n.d(c,{Fqs:function(){return u}});var h=n(4405);function u(t){return(0,h.w_)({tag:"svg",attr:{viewBox:"0 0 24 24"},child:[{tag:"path",attr:{d:"M16 1H4c-1.1 0-2 .9-2 2v14h2V3h12V1zm3 4H8c-1.1 0-2 .9-2 2v14c0 1.1.9 2 2 2h11c1.1 0 2-.9 2-2V7c0-1.1-.9-2-2-2zm0 16H8V7h11v14z"}}]})(t)}}}]);
